08b44394e3b5065256d3971159b7deb669918e0d
coding-or-afk <seodw1107@hanmail.net>
2020-01-13 15:28:11 +0900

    Add files via upload

---
+++ sdwWPFPractice/CustomForms/MsgControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomForms
+{
+    public static class MsgControl
+    {
+        public static void show(string msg)
+        {
+            msg1 uc = new msg1();
+            uc.tb1.Text = msg;
+            uc.ShowDialog();
+        }
+    }
+}
---
+++ sdwWPFPractice/sdwWPFPractice/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using CustomForms;
+
+namespace sdwWPFPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomForms.MsgControl.show(button1.Text);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CustomForms.MsgControl.show(button2.Text);
+        }
+    }
+}
